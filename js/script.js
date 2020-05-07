b0e9ea3adf061a0ac874ea7e0a1ff20fe8f24502
Pepzini <52154339+Pepzini@users.noreply.github.com>
2020-05-07 18:12:15 +0100

    script change

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 function validateform(params) {
-    if (document.myForm.fullName.value=='') {
+    if (document.myForm.fullName.value == '') {
         alert('Name cannot be empty');
         document.myForm.name.focus();
     }
-   if (document.myForm.email.value=='') {
-      alert('Email cannot be empty');
-      document.myForm.email.focus();
-      return false; 
-   } 
-   if (document.myForm.password.length<6) {
-      alert('Password too short')
-      document.myForm.password.focus();
-      return false; 
-   } if (document.myForm.password.value != document.myForm.confirmPassword.value) {
-       alert('Password do not match')
-   } else {
-    alert("Password Match: Welcome to GoGirl") 
-    return true; 
-   }
+    if (document.myForm.email.value == '') {
+        alert('Email cannot be empty');
+        document.myForm.email.focus();
+        return false;
+    }
+    if (document.myForm.password.value.length < 6) {
+    alert('Password too short');
+    document.myForm.password.focus();
+    return false;
+    } if (document.myForm.password.value != document.myForm.confirmPassword.value) {
+    alert('Password do not match')
+    } else {
+    alert("Password Match: Welcome to GoGirl")
+    return true;
+}
 }
